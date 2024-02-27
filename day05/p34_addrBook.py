71eb5ced8fd78fa6356d237460887cc7c80fafaf
Seonghohoho <bsh5220@naver.com>
2024-02-27 11:47:15 +0900

    주소록 수정 계속

--- day05/p34_addrBook.py
+++ day05/p34_addrBook.py
@@ -1,28 +1,75 @@
-# file : p34_addrBook.py
-# desc : 콘솔 주소록  프로그램
+# file: p34_addrBook.py
+# desc: 콘솔 주소록  프로그램
 
-class Contact: # 주소록 클래스
-    def __init__(self,name,phoneNumber,eMail,addr) -> None:
+import os
+
+class Contact:
+    def __init__(self, name, phoneNumber, eMail, addr) -> None:
         self.__name = name
         self.__phoneNumber = phoneNumber
         self.__eMail = eMail
         self.__addr = addr
 
-    # 사용자가 원하는 형태로 출력 
-    def __str__(self) -> str: # 원래출력 <__main__.Coontact object at 0x0000024500772150>
+    def __str__(self) -> str:
         res = (f'이  름 : {self.__name}\n' 
                f'핸드폰 : {self.__phoneNumber}\n'
                f'이메일 : {self.__eMail}\n'
                f'주  소 : {self.__addr}')
         return res
+    
+def setContact():
+    (name, phoneNumber, eMail, addr) = input('주소록 입력(이름, 핸드폰, 이메일, 주소[구분자 /]) > ').split('/')
+    name = name.strip()
+    phoneNumber = phoneNumber.strip()
+    eMail = eMail.strip()
+    addr = addr.strip()
+    contact = Contact(name, phoneNumber, eMail, addr)
+    return contact
+
+def clearConsole():
+    cmd = 'clear'
+    if os.name in ('nt', 'dos'):
+        cmd = 'cls'
+    os.system(cmd)
+
+def displayMenu():
+    menu = ('주소록 프로그램\n'
+            '1. 연락처 추가\n'
+            '2. 연락처 출력\n'
+            '3. 연락처 삭제\n'
+            '4. 종료\n')
+    print(menu)
+    sel = int(input('메뉴입력 : '))
+    return sel
+
+def getContact(lstContact):
+    for contact in lstContact:
+        print(contact)
 
 def run():
-    # first = Contact('홍길동', '010-9999-5555', 'hgd@naver.com', '경성')
-    first = Contact(addr='경성', phoneNumer= '010-9999-5555' , name ='홍길동', eMail='hgd@naver.com')
-    print(first)
+    lstContact = []
 
-if __name__ == '__main__': # 메인엔트리
+    clearConsole()
+    while True:
+        selMenu = displayMenu()
+
+        if selMenu == 1:
+            clearConsole()
+            contact = setContact()
+            lstContact.append(contact)
+            input()
+            clearConsole() 
+        elif selMenu == 2:
+            clearConsole()
+            getContact(lstContact)
+            input()
+            clearConsole()
+        elif selMenu == 4:
+            break
+        else:
+            clearConsole()
+
+if __name__ == '__main__':
     print('프로그램 시작')
-    run() # 메인함수 실행
-
-print('프로그램 종료')
+    run()
+    print('프로그램 종료')
